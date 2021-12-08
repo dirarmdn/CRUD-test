1ae1a794909d4b2e1e9e74405a6e3d09a1f24187
Dhira Ramadini <dhiraramadini@gmail.com>
2021-12-08 17:51:54 +0700

    Menambahkan file style.css dan mengedit index.html

--- index.php
+++ index.php
@@ -1,50 +1,49 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Membuat CRUD Dengan PHP Dan MySQL - Menampilkan data dari database</title>
+	<title>Membuat CRUD Dengan PHP Dan MySQL - Menampilkan data dari database</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    <div class="judul">
-        <h1>Membuat CRUD Dengan PHP Dan MySQL</h1>
-        <h2>Menampilkan data dari database</h2>
-        <h3>#praktekdhira</h3>
-</div>
-<br/>
+	<div class="judul">		
+		<h1>Membuat CRUD Dengan PHP Dan MySQL</h1>
+		<h2>Menampilkan data dari database</h2>
+		<h3>www.malasngoding.com</h3>
+	</div>
+	<br/>
 
-<?php
-if(isset($_GET['pesan'])) {
-    $pesan = $_GET['pesan'];
-    if($pesan == "input") {
-        echo "Data berhasil diinput.";
-    }else if($pesan == "update"){
-        echo "Data berhasil diupdate.";
-    }else if($pesan == "hapus"){
-        echo "Data berhasil dihapus.";
-    }
-    ?>
-    <br/>
-    <a class="tombol" href="input.php">+ Tambah Data Baru</a>
+	<?php 
+	if(isset($_GET['pesan'])){
+		$pesan = $_GET['pesan'];
+		if($pesan == "input"){
+			echo "Data berhasil di input.";
+		}else if($pesan == "update"){
+			echo "Data berhasil di update.";
+		}else if($pesan == "hapus"){
+			echo "Data berhasil di hapus.";
+		}
+	}
+	?>
+	<br/>
+	<a class="tombol" href="input.php">+ Tambah Data Baru</a>
 
-    <h3>Data user</h3>
-    <table border="1" class="table">
-        <tr>
-            <th>No</th>
-            <th>Nama</th>
-            <th>Alamat</th>
-            <th>Pekerjaan</th>
-            <th>Opsi</th>
-</tr>
-<?php
-include "koneksi.php";
-$query_mysql = mysql_query("SELECT * FROM user") or die(mysql_error());
-$nomor = 1;
-while($data = mysql_fetch_array($query_mysql)) {
-    ?>
-    <tr>
-    <td><?php echo $nomor++; ?></td>
+	<h3>Data user</h3>
+	<table border="1" class="table">
+		<tr>
+			<th>No</th>
+			<th>Nama</th>
+			<th>Alamat</th>
+			<th>Pekerjaan</th>
+			<th>Opsi</th>		
+		</tr>
+		<?php 
+		include "koneksi.php";
+		$query_mysql = mysql_query("SELECT * FROM user")or die(mysql_error());
+		$nomor = 1;
+		while($data = mysql_fetch_array($query_mysql)){
+		?>
+		<tr>
+			<td><?php echo $nomor++; ?></td>
 			<td><?php echo $data['nama']; ?></td>
 			<td><?php echo $data['alamat']; ?></td>
 			<td><?php echo $data['pekerjaan']; ?></td>
